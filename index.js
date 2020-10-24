46926a8d0871b6423395d96318c416c5b7008616
Ivana Radalj <iradalj@pmfst.hr>
2020-10-24 14:58:58 +0200

    mogoose baza

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = express()
 const cors = require('cors')
 app.use(cors())
@@ -7,7 +6,21 @@ app.use(express.json())
 //midlever za slanje staticke stranice na server
 //build mapa mi sadrzi taj staticki sadrzaj
 app.use(express.static('build'))
-	
+//model Poruka
+const Poruka=require('./models/poruke')
+const { response } = require('express')
+
+const novaporuka = new Poruka({
+    ime:'mongoose ime',
+    prezime: 'mongooseprezime',
+    email : 'mongoose prezime'
+
+})
+
+
+
+
+	//normalni middlever prima 3 parametra
 const zahtjevInfo = (req, res, next) => {
     console.log('Metoda:', req.method)
     console.log('Putanja:', req.path)
@@ -42,59 +55,110 @@ app.get('/',(req,res) => {
 
 })
 app.get('/api/poruke',(req,res) => {
-    res.json(poruke)
+    Poruka.find({}).then(svePoruke => {
+        res.json(svePoruke)
+    })
 
 })
-app.get('/api/poruke/:id',(req,res) => {
-    const id=Number(req.params.id) 
-    const poruka = poruke.find(p => p.id === id)
-    if(poruka){
-        res.json(poruka)
+app.get('/api/poruke/:id',(req,res,next) => {
+    //za bazu
+    const id=req.params.id
+    Poruka.findById(id)
+    .then(poruka => {
+        //ako je ispravan format id-a
+        if(poruka){
+            res.json(poruka)
+        }
+        else{
+            //da ne izbaci null jer podatak s tim idem koji se rzalikuje u zadnjoj znamenci postoji ali je null
+
+            res.status(404).end()
+        }
+    })
+    //ako opce nie ispravan format id-a
+    //u cijeloj app trazi onaj middlever sa 4 argumenta,skace na onu dole errorhendler funkciju
+
+    .catch(err => next(err))
 
 
-    }
-    else{
-        res.status(404).end()
-    }
+
+
+    // const id=Number(req.params.id) 
+    // const poruka = poruke.find(p => p.id === id)
+    // if(poruka){
+    //     res.json(poruka)
+
+
+    // }
+    // else{
+    //     res.status(404).end()
+    // }
+
+    
+
 
 })
 app.delete('/api/poruke/:id',(req,res) => {
-    const id=Number(req.params.id) 
-    poruke=poruke.filter(p => p.id !== id)
-    res.status(204).end()
+    const id=req.params.id
+    Poruka.findByIdAndRemove(id)
+    .then(result =>{
+        res.status(204).end()
+    })
+    .catch(err => next(err))
    
 
 })
 
-app.post('/api/poruke' ,(req,res)=> {
-    const maxid = poruke.length > 0 
-    ? Math.max(...poruke.map(p => p.id))
-    : 0
+app.post('/api/poruke' ,(req,res,next)=> {
+    // const maxid = poruke.length > 0 
+    // ? Math.max(...poruke.map(p => p.id))
+    // : 0
 
-
+    //podatak kojeg upisujemo na frontendu
     const podatak=req.body
-    if(!podatak.ime || !podatak.prezime || !podatak.email){
-        return res.status(400),json({
-            error:'nedostaje sadrzaj'
-        })
-    }
-    const poruka = {
+    // if(!podatak.ime || !podatak.prezime || !podatak.email){
+    //     return res.status(400),json({
+    //         error:'nedostaje sadrzaj'
+    //     })
+    // }
+    const poruka = new Poruka({
+        ime:podatak.ime,
+        prezime:podatak.prezime,
+        email:podatak.email
+    })
+    // poruka.id=maxid+1
+
+    // poruke=poruke.concat(poruka)
+    // res.json(poruka)
+
+    //logika rada s  bazom
+    poruka.save().then(result => {
+        console.log("podatak spremljen");
+        res.json(result)
+    })
+    .catch(err => next(err))
+
+})
+app.put('/api/poruke/:id',(req,res,next) => {
+    const id=req.params.id
+    const podatak= req.body
+    // podatak.id=Number(req.params.id)
+    // poruke=poruke.map(p => p.id !== id ? p : podatak)
+    // res.json(podatak)
+    // res.status(204).end()
+
+    //baza
+
+    const poruka= {
         ime:podatak.ime,
         prezime:podatak.prezime,
         email:podatak.email
     }
-    poruka.id=maxid+1
 
-    poruke=poruke.concat(poruka)
-    res.json(poruka)
-})
-app.put('/api/poruke/:id',(req,res) => {
-    const id=Number(req.params.id) 
-    const podatak= req.body
-    podatak.id=Number(req.params.id)
-    poruke=poruke.map(p => p.id !== id ? p : podatak)
-    res.json(podatak)
-    res.status(204).end()
+    Poruka.findByIdAndUpdate(id,poruka,{new: true}).then(poruka => {
+        res.json(poruka)
+    })
+    .catch(err => next(err))
 
    
 
@@ -105,6 +169,26 @@ const nepoznataRuta = (req, res) => {
   }
    
   app.use(nepoznataRuta)
+
+
+  //error hendler middlewear funkcije,primaju 4 argumenta, a ne 3 kao klasicni middlveri
+const errorHandler = (err,req,res,next) =>{
+      console.log("middleweare za pogreske")
+      if(err.name= "CastError"){
+          return res.status(400).send({error:"krivi fformat id parametra"})
+
+      }else if (err.name === "MongoParseError"){
+        return res.status(400).send({error:"krivi format podatka"})
+
+      }
+      next(err)
+  }
+  
+
+app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
---
+++ models/poruke.js
@@ -0,0 +1,62 @@
+//sve vezano uz bazu ide u mapu models
+const mongoose= require('mongoose')
+const password = process.env.ATLAS_PASS
+const dbname= 'porukeapi'
+const url=`mongodb+srv://ivanaradalj-okviri:${password}@cluster0.vqn5c.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
+console.log("spajam se na bazu")
+//connect je isto promise,callbek pa ide .then i .catch
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }).then(result => {
+    console.log("Spojeni smo na bazu");
+  }).catch(error => {
+    console.log("Greška pri spajanju", error.message);
+  })
+
+
+
+  //shema
+  //mongo baca validation error kada pokusam spremiti podatak koji ne odgovara validaciji 
+  
+  const porukaSchema = new mongoose.Schema({
+      //validacija sheme
+    ime:{
+      type: String,
+      required: true,
+      minlength: 5 
+    },
+    prezime: {
+      type: String,
+      required: true,
+      minlength: 5 
+
+    },
+    email:{
+      type: String,
+      required: true,
+      minlength: 5 
+
+    }
+
+
+})
+//radi nonoga id-a ruznog i onoga _v,overrajdat cemo metodu .tojson
+//doc je kako inace vrati,a ret je kako mi zelimo da vrati
+porukaSchema.set('toJSON',{
+    transform: (doc,ret) => {
+        ret.id= ret._id.toString()
+        delete ret._id //nezelim id dami vrati
+        delete ret.__v //nezelim da mi vrati v
+        return ret
+
+
+    }
+})
+
+
+//model kojeg moram exportat
+module.exports = mongoose.model('Poruka',porukaSchema, 'poruke')
